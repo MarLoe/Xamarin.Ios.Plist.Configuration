a042725fa3d158b0acc3cb7fbcf564788901c3c6
Martin Løbger <martin@lobger.com>
2022-08-04 14:59:40 +0200

    Microsoft Visual Studio extension for adding plist configurations

---
+++ Xamarin.Ios.Plist.Configuration.addin/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "Xamarin.Ios.Plist.Configuration",
+    Namespace = "VisualStudioMac",
+    Version = "17.0",
+    Category = "IDE extensions"
+)]
+
+[assembly: AddinName("Xamarin Ios Plist Configuration")]
+[assembly: AddinDescription("Create configuration specific plist settings.\n\nby Martin Løbger")]
+[assembly: AddinAuthor("Martin Løbger")]
+[assembly: AddinUrl("https://github.com/MarLoe/Xamarin.Ios.Plist.Configuration")]
+
+[assembly: AddinDependency("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)] 
